dbab6e2e4d4e4ee3af116eb33196f61c1f4c6bc8
haight <gao_ht@126.com>
2022-08-14 00:03:23 +0800

    应用框架

--- src/main.js
+++ src/main.js
@@ -1,9 +1,41 @@
 const express = require('express')
-
 const app = express()
-
 const port = 3000
 
 app.listen(port, () => {
   console.log(`服务已启动，端口${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间',
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小',
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝',
+  },
+]
+
+app.get('/posts', (req, res) => {
+  res.send(data)
+})
+
+app.get('/posts/:postId', (req, res) => {
+  const { postId } = req.params
+
+  const item = data.find((item) => item.id == postId)
+
+  res.send(item)
 })
